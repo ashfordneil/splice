365c7adaabaf9400dfdb79b1757b8aac2f5aa60c
Neil Ashford <ashfordneil0@gmail.com>
2016-12-10 18:59:46 +1000

    started cleaning the command line arguments

--- src/setup.rs
+++ src/setup.rs
@@ -5,6 +5,7 @@ use regex;
 use std::io::BufReader;
 use std::fs::File;
 use regex::Regex;
+use clap::{Arg, App};
 
 /// A combination of the various error types that can arise during setup.
 #[derive(Debug)]
@@ -32,26 +33,41 @@ pub struct Options {
     stop: Regex,
     /// A buffered reader of the input file.
     input: BufReader<Box<io::Read>>,
+    // /// Is the program to operate in recursive mode.
+    // recursive: bool,
 }
 
 impl Options {
     /// Parses command line arguments, and returns the Options struct (in a Result wrapper).
     pub fn setup() -> Result<Options, Error> {
-        let matches = clap_app!(splice =>
-            (version: "0.2.0")
-            (author: "Neil Ashford <ashfordneil0@gmail.com>")
-            (about: "A flexible, regex based command line filter")
-            (@arg START_REGEX: +required "The regex to start the filter at")
-            (@arg STOP_REGEX: +required "The regex to stop the filter at")
-            (@arg INPUT: "The input file to read from (defaults to stdin)")
-        )
+        let matches = App::new("splice")
+            .version("0.2.0")
+            .author("Neil Ashford <ashfordneil0@gmail.com>")
+            .about("A flexible, regex based command line filter")
+            .arg(Arg::with_name("open_regex")
+                .required(true)
+                .help("The OPEN regex. Matches the (optionally recursive) entrance to the \
+                       splice."))
+            .arg(Arg::with_name("close_regex")
+                .required(true)
+                .help("The CLOSE regex. Matches the (optionally recursive) exit to the splice."))
+            .arg(Arg::with_name("input")
+                .required(false)
+                .value_name("FILE")
+                .help("The file to splice. Defaults to stdin."))
+            .arg(Arg::with_name("recursive")
+                .required(false)
+                .short("r")
+                .long("recursive")
+                .help("Applies splice recursively. Each match of the OPEn regex pushes a new \
+                       layer onto a stack, and each match of the CLOSE regex pops the previous \
+                       layer off of the stack."))
             .get_matches();
 
-        let start = try!(Regex::new(matches.value_of("START_REGEX").unwrap())
-            .map_err(Error::Regex));
-        let stop = try!(Regex::new(matches.value_of("STOP_REGEX").unwrap()).map_err(Error::Regex));
+        let start = try!(Regex::new(matches.value_of("open_regex").unwrap()).map_err(Error::Regex));
+        let stop = try!(Regex::new(matches.value_of("close_regex").unwrap()).map_err(Error::Regex));
 
-        let input: BufReader<Box<io::Read>> = match matches.value_of("INPUT") {
+        let input: BufReader<Box<io::Read>> = match matches.value_of("input") {
             Some(filename) => {
                 let file = try!(File::open(filename).map_err(Error::Io));
                 BufReader::new(Box::new(file))
@@ -62,10 +78,13 @@ impl Options {
             }
         };
 
+        // let recursive = match matches.value_of("recursive").unwrap();
+
         Ok(Options {
             start: start,
             stop: stop,
             input: input,
+            // recursive: recursive,
         })
     }
 
